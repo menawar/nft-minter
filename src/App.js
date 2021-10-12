fd9de4ce960d3c3f06b94b99d9530811bb2d9342
Sylvester Menawar <sylvesternathan93@gmail.com>
2021-10-12 12:11:11 +0000

    added signature canvas

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { fetchData } from "./redux/data/dataActions";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
 import { create } from "ipfs-http-client";
+import SignatureCanvas from "react-signature-canvas";
+
+const ipfsClient = create("https://ipfs.infura.io:5001/api/v0");
 
 export const StyledButton = styled.button`
   padding: 8px;
@@ -14,6 +17,69 @@ function App() {
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("");
+  const [NFTS, setNFTS] = useState([]);
+  const elementRef = useRef();
+  const ipfsBaseUrl = "https://ipfs.infura.io/ipfs/";
+  const name = "NFT name";
+  const description = "IPFS minted nft woooooo.";
+
+  const mint = (_uri) => {
+    blockchain.smartContract.methods
+      .mint(blockchain.account, _uri)
+      .send({ from: blockchain.account })
+      .once("error", (err) => {
+        console.log(err);
+        setLoading(false);
+        setStatus("Error");
+      })
+      .then((receipt) => {
+        console.log(receipt);
+        setLoading(false);
+        clearCanvas();
+        dispatch(fetchData(blockchain.account));
+        setStatus("Successfully minting your NFT");
+      });
+  };
+
+  const createMetaDataAndMint = async (_name, _des, _imgBuffer) => {
+    // setLoading(true);
+    // setStatus("Uploading to IPFS");
+    try {
+      const addedImage = await ipfsClient.add(_imgBuffer);
+      console.log(ipfsBaseUrl + addedImage.path);
+      // const metaDataObj = {
+      //   name: _name,
+      //   description: _des,
+      //   image: ipfsBaseUrl + addedImage.path,
+      // };
+      // const addedMetaData = await ipfsClient.add(JSON.stringify(metaDataObj));
+      // console.log(ipfsBaseUrl + addedMetaData.path);
+      // mint(ipfsBaseUrl + addedMetaData.path);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setStatus("Error");
+    }
+  };
+
+  const startMintingProcess = () => {
+    createMetaDataAndMint(name, description, getImageData());
+  };
+
+  const getImageData = () => {
+    const canvasEl = elementRef.current;
+    let dataUrl = canvasEl.toDataURL("image/png");
+    const buffer = Buffer(dataUrl.split(",")[1], "base64");
+    console.log(buffer);
+    return buffer;
+  };
+
+  const clearCanvas = () => {
+    const canvasEl = elementRef.current;
+    canvasEl.clear();
+  };
 
   useEffect(() => {
     if (blockchain.account !== "" && blockchain.smartContract !== null) {
@@ -43,8 +109,34 @@ function App() {
       ) : (
         <s.Container flex={1} ai={"center"} style={{ padding: 24 }}>
           <s.TextTitle style={{ textAlign: "center" }}>
-            Name: {data.name}.
+            Welcome mint your signature
           </s.TextTitle>
+          <s.SpacerLarge />
+          <s.Container fd={"row"} jc={"center"}>
+            <StyledButton
+              onClick={(e) => {
+                e.preventDefault();
+                startMintingProcess();
+              }}
+            >
+              MINT
+            </StyledButton>
+            <s.SpacerSmall />
+            <StyledButton
+              onClick={(e) => {
+                e.preventDefault();
+                clearCanvas();
+              }}
+            >
+              CLEAR
+            </StyledButton>
+          </s.Container>
+          <s.SpacerLarge />
+          <SignatureCanvas
+            backgroundColor={"#3271bf"}
+            ref={elementRef}
+            canvasProps={{ width: 350, height: 350 }}
+          />
         </s.Container>
       )}
     </s.Screen>
